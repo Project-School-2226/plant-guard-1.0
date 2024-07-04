24227723c39dfaef1eb2c6f748bc94ad72f50b3e
susheel_t <susheel.tatikonda@gmail.com>
2024-07-04 15:10:59 +0530

    test contribution

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:chat_gpt_chat_app/chat_page.dart';
 import 'package:flutter/material.dart';
- 
+// import goats
+
 void main() async {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
