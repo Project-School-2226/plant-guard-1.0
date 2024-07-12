d4f4e9197f75c03451a539837a0481fbcfe0b393
Shaik Amaan <shaikamaanahmed2585@gmail.com>
2024-07-12 16:52:25 +0530

    I think this solves it

--- backend/esp32/sensors.ino
+++ backend/esp32/sensors.ino
@@ -5,7 +5,7 @@
 #include <DHTesp.h>
 using namespace std;
 
-//definitions for pin numbers 
+// Definitions for pin numbers 
 const int soilHumidityPin = 34; // Define the analog pin for the soil moisture sensor
 const int atmTemp = 5;
 const int dryValue = 2000; // Define the analog value when the soil is dry
@@ -16,51 +16,52 @@ DHTesp dht;
 
 const char* ssid = "Test";
 const char* password = "Welcome@123";
-const char* serverUrl = "http://172.168.0.106:64000/"; // Adjust the server URL
+const char* serverUrl = "http://172.168.0.69:3500/sensors-data/post-data-to-server"; // Adjust the server URL
 const unsigned long sendDataInterval = 5000; // Interval for sending data to server (milliseconds)
 
 // Variables to store sensor data
 float sensorData[6] = {0}; // Change the size and type according to your sensor data
-WebServer server(30000); // Define a WebServer instance listening on port 80
-// Function to format data into JSON
+WebServer server(80); // Define a WebServer instance listening on port 80
 
-
-//function prototypes
+// Function prototypes
 float handleMoisturePer(float);
 
 class SensorValues {
-  private:se_p moisture_pin;
-  private:se_p temperature_pin;
+  private: 
+    se_p moisture_pin;
+    se_p temperature_pin;
 
-  public:SensorValues(se_p humidity_pin, se_p temperature_pin) {
-    this->moisture_pin = moisture_pin;
-    this->temperature_pin = temperature_pin;
-  }
+  public: 
+    SensorValues(se_p humidity_pin, se_p temperature_pin) {
+      this->moisture_pin = moisture_pin;
+      this->temperature_pin = temperature_pin;
+    }
 
-  float getTemperature() {
-    return dht.getTemperature();
-  }
+    float getTemperature() {
+      return dht.getTemperature();
+    }
 
-  int getSoilMoisture(){
-    return analogRead(soilHumidityPin);
-  }
+    float getHumidity() {
+      return dht.getHumidity();
+    }
 
+    int getSoilMoisture() {
+      return analogRead(soilHumidityPin);
+    }
 };
 
-String formatData(float N, float P, float K, float temp, float humidity, float pH) {
+String formatData(float N, float P, float K, float temp, float soilMoisture, float humidity) {
   StaticJsonDocument<200> doc;
   doc["N"] = N;
   doc["P"] = P;
   doc["K"] = K;
   doc["temperature"] = temp;
+  doc["soilMoisture"] = soilMoisture;
   doc["humidity"] = humidity;
-  doc["pH"] = pH;
   String jsonString;
   serializeJson(doc, jsonString);
   return jsonString;
 }
-
-
 
 // Function to send data to server
 void sendDataToServer() {
@@ -84,23 +85,20 @@ void sendDataToServer() {
     http.end();
   }
 }
+
 SensorValues sens_vals(soilHumidityPin, atmTemp);
 
-
-void handleGetRequest(){
+void handleGetRequest() {
   String responseMessage = "This is a GET request!";
 
-  String jsonData = formatData(sensorData[0], sensorData[1], sensorData[2], sens_vals.getTemperature(), handleMoisturePer(sens_vals.getSoilMoisture()), sensorData[5]);
+  String jsonData = formatData(sensorData[0], sensorData[1], sensorData[2], sens_vals.getTemperature(), handleMoisturePer(sens_vals.getSoilMoisture()), sens_vals.getHumidity());
   server.send(200, "application/json", jsonData);
 }
 
 float handleMoisturePer(float val) {
-
   int moisturePercentage = map(val, dryValue, wetValue, 0, 100);
   moisturePercentage = constrain(moisturePercentage, 0, 100); // Ensure the percentage stays within 0-100
-
   return moisturePercentage;
-
 }
 
 void setup() {
@@ -111,22 +109,19 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-    Serial.println("Connected to WiFi");
-    Serial.print("ESP32 IP Address: ");
-    Serial.println(WiFi.localIP());
-      
-    server.on("/get-data", HTTP_GET, handleGetRequest);
+  Serial.println("Connected to WiFi");
+  Serial.print("ESP32 IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/get-data", HTTP_GET, handleGetRequest);
   // Start the HTTP server
-    server.begin();
-    
-    
+  server.begin();
 }
 
-
 void loop() {
-
   float temperature = sens_vals.getTemperature();
   int soilMoisture = sens_vals.getSoilMoisture();
+  float humidity = sens_vals.getHumidity();
 
   int moisturePercentage = map(soilMoisture, dryValue, wetValue, 0, 100);
   moisturePercentage = constrain(moisturePercentage, 0, 100); // Ensure the percentage stays within 0-100
@@ -134,6 +129,8 @@ void loop() {
   // Update sensor data array
   sensorData[3] = temperature; // Assuming temperature is stored at index 3
   sensorData[4] = moisturePercentage; // Assuming soil moisture is stored at index 4
+  sensorData[5] = humidity; // Assuming humidity is stored at index 5
+
   // Send data to server periodically
   static unsigned long lastSendTime = 0;
   unsigned long currentTime = millis();
@@ -143,5 +140,5 @@ void loop() {
   }
 
   server.handleClient();  
-// Other code here as needed
+  // Other code here as needed
 }
