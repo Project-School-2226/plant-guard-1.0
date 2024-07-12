be7a87f4450a842b10e7f94730ca951c2db6e4c1
Shaik Amaan <shaikamaanahmed2585@gmail.com>
2024-07-12 11:01:47 +0530

    test commit 1

--- backend/server/README.md
+++
@@ -1,1 +0,0 @@
-# plant-guard-server
--- backend/server/routes/sensors_data.js
+++ backend/server/routes/sensors_data.js
@@ -1,16 +1,17 @@
 const express  = require('express');
 const router = express.Router();
-const getFromEsp32 = require('../utils/sensorFunctions');
+const  {getFromEsp32} = require('../utils/sensorFunctions');
+const http = require('http');
 
 router.post('/post-data-to-server' , (req,res) => {
 	const data = req.body;
-	console.log("Data received " + data.N,data.P,data.K,data.temperature,data.humidity,data.pH);
+	console.log("Data received " + data.N,data.P,data.K,data.temperature,data.soilMoisture,data.humidity);
 	res.status(200);
 })
 
 router.get('/get-from-esp32', (req, res) => {
     try {
-      getFromEsp32();
+      getFromEsp32(req,res);
     }
     catch (error) {
       console.error(`Error getting data from ESP32: ${error}`);
